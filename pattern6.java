7c960e958aca9336c0c8c8b5e28dcd4b393b9f44
Siddhant Satkar <110604084+Siddhant-satkar@users.noreply.github.com>
2023-07-29 09:45:30 +0530

    Add files via upload

---
+++ pattern6.java
@@ -0,0 +1,18 @@
+class Main{
+
+	public static void main(String [] args){
+	
+		int num = 1;
+		for(int i=1;i<=3;i++){
+		
+			for(int j=1;j<=i;j++){
+				
+					System.out.print(num);
+					num=num+2;
+			}
+			System.out.println("");
+			}
+			
+			
+		}
+	}
---
+++ pattern8.java
@@ -0,0 +1,16 @@
+class Main{
+
+	public static void main(String args []){
+	
+	//	char ch = 65;
+		for(char i=65;i<=68;i++){
+		
+			for(char j=65;j<=i;j++){
+			
+				System.out.print(j);
+	
+			}
+			System.out.println("");
+		}
+	}
+}
