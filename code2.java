9f2f9221ee12bb9ddb04e455120015a1a37439d2
Siddhant Satkar <110604084+Siddhant-satkar@users.noreply.github.com>
2023-08-02 23:55:02 +0530

    Add files via upload

---
+++ code2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String[] args){
+	
+		float a = 10.52f;
+		float b = 11.22f;
+		float c = 20.34f;
+
+		float bill = a+b+c;
+
+		System.out.println(bill);
+	
+	}
+}
