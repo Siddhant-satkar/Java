05e61964591ea56035b7aac4ead06e905dc96bc9
Siddhant Satkar <110604084+Siddhant-satkar@users.noreply.github.com>
2023-08-04 08:17:53 +0530

    Add files via upload

---
+++ method11.java
@@ -0,0 +1,15 @@
+class Main{
+
+	void fun(int a){
+	
+		System.out.println("in fun");
+		System.out.println(a);
+	}
+	public static void main(String args []){
+	
+		Main obj = new Main();
+		obj.fun(10);
+
+	}
+}
+
