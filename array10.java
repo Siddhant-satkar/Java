10bf4a6ea75c2a4a4908e7f0cdf9f4aa29869d75
Siddhant Satkar <110604084+Siddhant-satkar@users.noreply.github.com>
2023-08-26 20:27:26 +0530

    Add files via upload

---
+++ array10.java
@@ -0,0 +1,28 @@
+class Main{
+
+	public static void reverse(int arr[]){
+	
+		int first=0;
+		int end = arr.length - 1;
+
+		while(first<end){
+		int temp = arr[end];
+		arr[end] = arr[first];
+		arr[first] = temp;
+
+		first++;
+		end--;
+		}
+		
+	}
+	public static void main(String args []){
+	
+		int arr[] = {2,3,4,5,6};
+
+		reverse(arr);
+		for(int i=0; i<arr.length; i++){
+		
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
